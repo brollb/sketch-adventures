17c0daa51d52e9e8fe62497dd579ffe7694dda9d
Brian Broll <brian.broll@gmail.com>
2018-01-27 15:35:30 -0600

    Added drawing on the screen! woot, woot!

--- README.md
+++ README.md
@@ -4,16 +4,20 @@
 - How can we get the dirty part of the screen?
 - [x] Draw on the screen
 - [x] Move the stick figure with arrow keys
+- [x] Game update loop
+- [x] Draw the ground
 
-- [ ] Game update loop
-    - 
 - [ ] Draw a stick figure (use an image?)
-- [ ] Draw the ground
 - [ ] Add physics (should be provided by piston, I think)
 - [ ] Allow the user to draw on the screen
 - [ ] Get the user-drawn image
 - [ ] Call python classifier on the image
 - How can we get the dirty part of the screen?
+
+- Draw character on screen. Awesome idea
+- collision detection
+
+- drawing on the screen
 
 ## Feature List:
 - [ ] moving 2d platformer
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,13 @@ extern crate find_folder;
 extern crate gfx;
 extern crate gfx_graphics;
 
+extern crate vecmath;
+extern crate image;
+
 use piston_window::*;
-use piston::input::GenericEvent;
 use std::borrow::BorrowMut;
+use vecmath::*;
+use image::Rgba;
 
 
 use gfx_device_gl::{Resources, CommandBuffer};
@@ -28,7 +32,12 @@ struct Game {
     up_d: bool,
     down_d: bool,
     right_d: bool,
-    left_d: bool
+    left_d: bool,
+
+    // Drawing
+    is_drawing: bool,
+    last_pos: Option<[f64; 2]>,
+    pub canvas: Option<image::ImageBuffer<Rgba<u8>, Vec<u8>>>
 }
 
 impl Game {
@@ -43,7 +52,10 @@ impl Game {
             up_d: false,
             down_d: false,
             right_d: false,
-            left_d: false
+            left_d: false,
+            is_drawing: false,
+            last_pos: None,
+            canvas: None
         }
     }
 
@@ -54,6 +66,7 @@ impl Game {
         let speed = 100.0;
         self.enemy.update(dt);
 
+        // TODO: get the window encoder...
         /*
         if self.up_d {
             self.player.mov(0.0, speed * dt);
@@ -73,7 +86,21 @@ impl Game {
         }
     }
 
+    fn on_load(&mut self, w: &mut PistonWindow) {
+        let assets = find_folder::Search::ParentsThenKids(3, 3).for_folder("assets").unwrap();
+        let tank_sprite = assets.join("E-100_preview.png");
+        let tank_sprite = Texture::from_path(
+            &mut *w.factory.borrow_mut(),
+            &tank_sprite,
+            Flip::None,
+            &TextureSettings::new())
+            .unwrap();
+
+        self.player.set_sprite(tank_sprite);
+    }
+
     pub fn on_release(&mut self, input: piston::input::Button) {
+        // Player movement
         if let Button::Keyboard(Key::Left) = input {
             self.left_d = false;
         }
@@ -90,24 +117,16 @@ impl Game {
         if let Button::Keyboard(Key::Down) = input {
             self.down_d = false;
         }
-    }
 
-    fn on_load(&mut self, w: &mut PistonWindow) {
-        let assets = find_folder::Search::ParentsThenKids(3, 3).for_folder("assets").unwrap();
-        let tank_sprite = assets.join("E-100_preview.png");
-        let tank_sprite = Texture::from_path(
-            &mut *w.factory.borrow_mut(),
-            &tank_sprite,
-            Flip::None,
-            &TextureSettings::new())
-            .unwrap();
-        self.player.set_sprite(tank_sprite);
+        // Painting
+        if input == Button::Mouse(MouseButton::Left) {
+            println!("stopping drawing!");
+            self.is_drawing = false;
+        }
     }
 
     pub fn on_press(&mut self, input: piston::input::Button) {
-        // Update the game state
-        let speed = 5.0;
-
+        // Player movement
         if let Button::Keyboard(Key::Left) = input {
             self.left_d = true;
         }
@@ -123,6 +142,44 @@ impl Game {
         if let Button::Keyboard(Key::Down) = input {
             self.down_d = true;
         }
+
+        // Painting
+        if input == Button::Mouse(MouseButton::Left) {
+            let width = self.width as u32;
+            let height = self.height as u32;
+            println!("started drawing!");
+            self.is_drawing = true;
+            self.canvas = Some(image::ImageBuffer::new(width, height));
+        }
+    }
+
+    pub fn on_mouse_move(&mut self, pos: [f64; 2]) {
+        let width = self.width as u32;
+        let height = self.height as u32;
+
+        if self.is_drawing {
+            if let Some(ref mut canvas) = self.canvas {
+                let (x, y) = (pos[0] as f32, pos[1] as f32);
+
+                if let Some(p) = self.last_pos {
+                    let (last_x, last_y) = (p[0] as f32, p[1] as f32);
+                    let distance = vec2_len(vec2_sub(p, pos)) as u32;
+
+                    for i in 0..distance {
+                        let diff_x = x - last_x;
+                        let diff_y = y - last_y;
+                        let delta = i as f32 / distance as f32;
+                        let new_x = (last_x + (diff_x * delta)) as u32;
+                        let new_y = (last_y + (diff_y * delta)) as u32;
+                        if new_x < width && new_y < height {
+                            canvas.put_pixel(new_x, new_y, Rgba([0, 0, 0, 255]));
+                        };
+                    };
+                };
+
+                self.last_pos = Some(pos);
+            }
+        }
     }
 
     pub fn on_draw(&self, c: Context, g: &mut GfxGraphics<Resources, CommandBuffer>) {
@@ -130,6 +187,7 @@ impl Game {
         clear([1.0; 4], g);
         self.player.render(c, g);
         self.enemy.render(c, g);
+
 
         // Draw the ground
         rectangle([0.3, 0.3, 0.3, 1.0], // black
@@ -145,8 +203,13 @@ fn main() {
         .exit_on_esc(true).build().unwrap();
 
     let mut game = Game::new(width as f64, height as f64);
-    game.on_load(&mut window);
+    let mut texture = Texture::from_image(
+            &mut window.factory,
+            &image::ImageBuffer::new(width, height),
+            &TextureSettings::new()
+        ).unwrap();
 
+    game.on_load(&mut window);
     while let Some(e) = window.next() {
         // Split this into update and render events as done
         //  at http://piston-tutorial.logdown.com/posts/306682
@@ -159,12 +222,27 @@ fn main() {
             game.on_press(input);
         }
 
+        if let Some(ref canvas) = game.canvas {
+            texture = Texture::from_image(
+                    &mut window.factory,
+                    &canvas,
+                    &TextureSettings::new()
+                ).unwrap();
+        }
         window.draw_2d(&e, |c, g| {
+            // Detect drawing...
             game.on_draw(c, g);
+
+            // Display any drawing
+            image(&texture, c.transform, g);
         });
 
         if let Some(args) = e.update_args() {
             game.on_update(args);
+        }
+
+        if let Some(pos) = e.mouse_cursor_args() {
+            game.on_mouse_move(pos);
         }
     }
 }
